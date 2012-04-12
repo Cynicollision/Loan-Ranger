24c2af03d427b58f62e495afacf7ad20dd19e447
unknown <Sean@Mutiny.(none)>
2012-04-12 11:33:28 -0400

    Tab time.

--- gen/vast/loanranger/R.java
+++ gen/vast/loanranger/R.java
@@ -14,32 +14,35 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f050009;
-        public static final int contactsScrollView=0x7f05000a;
-        public static final int contactsTableLayout=0x7f05000b;
-        public static final int contactsTextView=0x7f050005;
-        public static final int editText1=0x7f050011;
-        public static final int linearLayout1=0x7f05000e;
-        public static final int newCaseButton=0x7f050007;
-        public static final int notesCancelButton=0x7f05000f;
-        public static final int notesEditText=0x7f05000d;
-        public static final int notesSaveButton=0x7f050010;
-        public static final int notesTextView=0x7f05000c;
-        public static final int overviewBackButton=0x7f050018;
-        public static final int overviewEditButton=0x7f050014;
-        public static final int overviewMapsButton=0x7f050017;
-        public static final int overviewNotesButton=0x7f050015;
-        public static final int overviewPhoneTextView=0x7f050012;
-        public static final int overviewPicturesButton=0x7f050016;
-        public static final int overviewSendButton=0x7f050019;
-        public static final int searchEditText=0x7f050003;
-        public static final int tableLayout=0x7f050001;
-        public static final int tableLayout1=0x7f050013;
-        public static final int tableRow0=0x7f050002;
-        public static final int tableRow1=0x7f050004;
-        public static final int tableRow2=0x7f050006;
-        public static final int tableRow3=0x7f050008;
-        public static final int textView1=0x7f050000;
+        public static final int button1=0x7f05000b;
+        public static final int contactsScrollView=0x7f05000c;
+        public static final int contactsTableLayout=0x7f05000d;
+        public static final int contactsTextView=0x7f050007;
+        public static final int editText1=0x7f050014;
+        public static final int formTab1Title=0x7f050000;
+        public static final int formTab2Title=0x7f050001;
+        public static final int formTab3Title=0x7f050002;
+        public static final int linearLayout1=0x7f050010;
+        public static final int newCaseButton=0x7f050009;
+        public static final int notesCancelButton=0x7f050011;
+        public static final int notesEditText=0x7f05000f;
+        public static final int notesSaveButton=0x7f050012;
+        public static final int notesTextView=0x7f05000e;
+        public static final int overviewBackButton=0x7f05001b;
+        public static final int overviewEditButton=0x7f050017;
+        public static final int overviewMapsButton=0x7f05001a;
+        public static final int overviewNotesButton=0x7f050018;
+        public static final int overviewPhoneTextView=0x7f050015;
+        public static final int overviewPicturesButton=0x7f050019;
+        public static final int overviewSendButton=0x7f05001c;
+        public static final int searchEditText=0x7f050005;
+        public static final int tableLayout=0x7f050003;
+        public static final int tableLayout1=0x7f050016;
+        public static final int tableRow0=0x7f050004;
+        public static final int tableRow1=0x7f050006;
+        public static final int tableRow2=0x7f050008;
+        public static final int tableRow3=0x7f05000a;
+        public static final int textView1=0x7f050013;
     }
     public static final class layout {
         public static final int form_main=0x7f030000;
@@ -65,6 +68,9 @@ public final class R {
         public static final int overview_pictures_button=0x7f04000b;
         public static final int overview_send_button=0x7f04000e;
         public static final int title_formeditor=0x7f040008;
+        public static final int title_formeditor_tab1=0x7f04000f;
+        public static final int title_formeditor_tab2=0x7f040010;
+        public static final int title_formeditor_tab3=0x7f040011;
         public static final int title_overview=0x7f040007;
     }
 }
